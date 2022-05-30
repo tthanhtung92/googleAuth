e8b7701cace36cc0373c22bb99974130068cbe49
Tung Tran <tthanhtung92@gmail.com>
2022-05-31 01:45:32 +0700

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
-import { signInWithGoogle} from "./Firebase"
+import "./App.css";
+import { signInWithGoogle } from "./Firebase";
+import GoogleButton from "react-google-button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button className="login-with-google-btn" onClick={signInWithGoogle} >Sign in with google</button>
-        <h1>{localStorage.getItem('name')}</h1>
-        <h1>{localStorage.getItem('email')}</h1>
-        <img src={localStorage.getItem('profilePicture')} />
+        <GoogleButton onClick={signInWithGoogle}></GoogleButton>
+        <h1>{localStorage.getItem("name")}</h1>
+        <h1>{localStorage.getItem("email")}</h1>
+        <img src={localStorage.getItem("profilePicture")} />
       </header>
     </div>
   );
